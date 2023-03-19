067ed69edc33a759cd8aeeffc5f99ef174d739f6
Bruna Ribeiro <brunaribeiro.ufv@gmail.com>
2023-03-19 11:05:50 -0300

    branch development create

---
+++ sidebar/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+
+  return (
+  <h1>SideBar</h1>
+  );
+}
